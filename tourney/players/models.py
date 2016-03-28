3f6ee9d4c86254ff2275334741854ee75b3ebe9d
Kevin Harvey <kharvey@axialhealthcare.com>
2016-03-27 21:30:51 -0500

    Adds Pool model

--- tourney/players/models.py
+++ tourney/players/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+
+
+class Pool(models.Model):
+    tournament = models.ForeignKey('matches.Tournament')
+    players = models.ManyToManyField('players.Player')
--- tourney/players/tests.py
+++ tourney/players/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 
 from model_mommy import mommy
 
-from players.models import Player
+from players.models import Player, Pool
+from matches.models import Tournament
 
 
 class PlayerTestCase(TestCase):
@@ -15,3 +16,17 @@ class PlayerTestCase(TestCase):
 
         self.assertIsNotNone(player.name)
         self.assertIsNotNone(player.email)
+
+
+class PoolTestCase(TestCase):
+
+    def test_basics(self):
+        """
+        Test the basic functionality of Pool
+        """
+        pool = mommy.make(Pool)
+        pool.players.add(mommy.make(Player))
+        pool.players.add(mommy.make(Player))
+
+        self.assertIsInstance(pool.tournament, Tournament)
+        self.assertEqual(pool.players.count(), 2)
