e813c7f4c9222e14a92c8748b357de3309743ea6
Kevin Harvey <kharvey@axialhealthcare.com>
2016-03-29 12:00:12 -0500

    Adds filtering to Match admin

--- tourney/matches/admin.py
+++ tourney/matches/admin.py
@@ -5,6 +5,7 @@ from matches.models import Tournament, Bracket, Round, Match, MatchNotification
 
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'round',)
+    list_filter = ('round__pool', 'round__number',)
 
 admin.site.register(Tournament)
 admin.site.register(Bracket)
