4c11ee3dcfd391c10b0514437a83b8476def8b29
Kevin Harvey <kharvey@axialhealthcare.com>
2016-03-29 12:33:53 -0500

    Adds RoundAdmin with list_display

--- tourney/matches/admin.py
+++ tourney/matches/admin.py
@@ -7,8 +7,12 @@ class MatchAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'round',)
     list_filter = ('round__pool', 'round__number',)
 
+
+class RoundAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'number', 'pool', 'start_datetime', 'end_datetime',)
+
 admin.site.register(Tournament)
 admin.site.register(Bracket)
-admin.site.register(Round)
+admin.site.register(Round, RoundAdmin)
 admin.site.register(Match, MatchAdmin)
 admin.site.register(MatchNotification)
